229143a130c89ace37fe17b64e7fb2b621ae61d7
till straumann <till.straumann@alumni.tu-berlin.de>
2017-11-02 13:42:47 -0700

    pathGrep updates

--- pathGrep.py
+++ pathGrep.py
@@ -3,16 +3,36 @@
 import pycpsw
 import re
 
-class myvisitor(pycpsw.PathVisitor):
-  def __init__(self, patt = None):
-    pycpsw.PathVisitor.__init__(self)
-    self.level=0
-    if patt != None:
-      self.re_prog = re.compile(patt)
-      self.result  = []
-    else:
-      self.re_prog = None
+# Subclass the PathVisitor.
+#
+# The user user is supposed to use this
+#
+# grep      = PathGrep(root, [needle])
+# found     = grep( needle )
+# foundFrom = grep( start, needle )
+#
+# any argument can be omitted in which case
+# the previous values are used.
+class PathGrep(pycpsw.PathVisitor):
+  """Recurse through CPSW hierarchy looking for RE pattern matches
 
+     Starts recursion at 'path' and returns a list of RE matches.
+  """
+  def __init__(self, root = None, patt = None):
+    pycpsw.PathVisitor.__init__(self)
+    self.level   = 0
+    self.result  = []
+    self.root    = root
+    self.setPatt_( patt )
+    self.maxl    = -1
+
+  def setPatt_(self, patt):
+    if patt == None:
+      self.re_prog = None
+    else:
+      self.re_prog = re.compile( patt )
+
+  # Visitor method we must implement for CPSW
   def visitPre(self, path):
     self.level = self.level + 1
     if self.re_prog == None:
@@ -27,20 +47,31 @@ class myvisitor(pycpsw.PathVisitor):
       print(st)
     elif self.re_prog.search( path.toString() ) != None:
       self.result.append(path.toString())
-    return True
+    return self.maxl < 0 or self.level < self.maxl
 
+  # Visitor method we must implement for CPSW
   def visitPost(self, path):
     self.level = self.level - 1
     pass
 
-def pgrep(path, re_pattern):
-  """Recurse through CPSW hierarchy looking for RE pattern matches
+  def getRoot(self):
+    return self.root
 
-     Starts recursion at 'path' and returns a list of RE matches.
-  """
-  v = myvisitor(re_pattern)
-  path.explore(v)
-  if re_pattern != None:
-    return v.result
-  else:
-    return None
+  def setRoot(self, root):
+    self.root = root
+
+  def __call__(self, patt = False, maxlevel = -1):
+    if maxlevel >=0:
+      self.maxl = maxlevel + 1
+    else:
+      self.maxl = -1
+    if patt or patt == None:
+      self.setPatt_( patt )
+    if self.root    == None:
+      raise Exception("No root set");
+    if self.re_prog != None:
+      self.result = []
+    else:
+      self.result = None
+    self.root.explore(self) 
+    return self.result
